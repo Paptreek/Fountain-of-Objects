8b576cb6f3c25992a63d5f6e9abd707b5c636820
packet-nova <90776963+packet-nova@users.noreply.github.com>
2026-02-21 12:39:28 -0500

    Player and room instantiation scripted

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Vector2 _playerStartingPosition = new Vector2(0, 0);
+    public GameObject RoomPrefab;
+    public GameObject PlayerPrefab;
+    public Room[,] Rooms;
+    public int RoomsHeight;
+    public int RoomsWidth;
+    public float Spacing;
+
+    void Start()
+    {
+        Debug.Log("Board is building");
+        Rooms = new Room[RoomsHeight, RoomsWidth];
+        BuildRooms();
+        PlayerPrefab = Instantiate(PlayerPrefab, _playerStartingPosition, Quaternion.identity);
+    }
+    public void BuildRooms()
+    {
+
+        for (float i = 0; i < RoomsWidth; i++)
+        {
+            for (float j = 0; j < RoomsHeight; j++)
+            {
+                Vector2 position = new(i * Spacing, j * Spacing);
+                GameObject room = Instantiate(RoomPrefab, position, Quaternion.identity);
+                Debug.Log($"New room placed at ({position})");
+            }
+        }
+    }
+}
+
