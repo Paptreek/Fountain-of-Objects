91debbc7974eed949d52f54f25e18e14c97143db
packet-nova <90776963+packet-nova@users.noreply.github.com>
2026-02-21 15:28:17 -0500

    Rooms are now child objects of RoomsManager Object

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : MonoBehaviour
     private Vector2 _playerStartingPosition = new Vector2(0, 0);
     public GameObject RoomPrefab;
     public GameObject PlayerPrefab;
+    public GameObject RoomsManager;
     public Room[,] Rooms;
     public int RoomsHeight;
     public int RoomsWidth;
@@ -25,7 +26,7 @@ public class GameManager : MonoBehaviour
             for (float j = 0; j < RoomsHeight; j++)
             {
                 Vector2 position = new(i * Spacing, j * Spacing);
-                GameObject room = Instantiate(RoomPrefab, position, Quaternion.identity);
+                GameObject room = Instantiate(RoomPrefab, position, Quaternion.identity, RoomsManager.transform);
                 Debug.Log($"New room placed at ({position})");
             }
         }
