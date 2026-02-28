d671ce31d006cbbac87f4242b8f3d2e5d310267a
packet-nova <90776963+packet-nova@users.noreply.github.com>
2026-02-28 11:20:25 -0500

    SpriteRenderer Component added to RoomPresenter

--- Assets/Scripts/Map/RoomPresenter.cs
+++ Assets/Scripts/Map/RoomPresenter.cs
@@ -4,8 +4,13 @@ namespace Assets.Scripts.Map
 {
     public class RoomPresenter : MonoBehaviour
     {
+        private SpriteRenderer _sprite;
         public RoomData Data { get; private set; }
 
+        void Awake()
+        {
+            _sprite = GetComponent<SpriteRenderer>();
+        }
 
         public void Initialize(RoomData data)
         {
