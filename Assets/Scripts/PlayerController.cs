ee932963e3660e10be1dde653623618d0a60831a
mattman111 <12969498+mattman111@users.noreply.github.com>
2026-06-06 10:22:33 -0400

    Add scale pulse and convert movement to Vector3
    Convert player movement start/target positions from Vector2 to Vector3 and added the ability to adjust scale during smooth movement for flair.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,20 +8,23 @@ public class PlayerController : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _mapAction;
 
-    [Header("Camera Manager")]
+    [Header ("Camera Manager")]
     [SerializeField] private CameraManager _cameraManager;
 
     [Header ("Smooth Movement")]
     [SerializeField] private float _moveDuration;
+    [SerializeField] private float _scaleMultiplier;
     private bool _isMoving = false;
-    private Vector2 _startPosition;
-    private Vector2 _targetPosition;
+    private Vector3 _startPosition;
+    private Vector3 _targetPosition;
     private float _elaspedTime = 0f;
+    private Vector3 _originScale;
 
     public void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
         _mapAction = InputSystem.actions.FindAction("OpenMap");
+        _originScale = transform.localScale;
 
     }
 
@@ -55,23 +59,24 @@ public class PlayerController : MonoBehaviour
             if (moveValue.x == 1)
             {
                 _startPosition = transform.position;
-                _targetPosition = new Vector2(transform.position.x + 2, transform.position.y);
+                _targetPosition = new Vector3(transform.position.x + 2, transform.position.y, transform.position.z);
             }
             else if (moveValue.x == -1)
             {
                 _startPosition = transform.position;
-                _targetPosition = new Vector2(transform.position.x - 2, transform.position.y);
+                _targetPosition = new Vector3(transform.position.x - 2, transform.position.y, transform.position.z);
             }
             else if (moveValue.y == 1)
             {
                 _startPosition = transform.position;
-                _targetPosition = new Vector2(transform.position.x, transform.position.y + 2);
+                _targetPosition = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
             }
             else if (moveValue.y == -1)
             {
                 _startPosition = transform.position;
-                _targetPosition = new Vector2(transform.position.x, transform.position.y - 2);
+                _targetPosition = new Vector3(transform.position.x, transform.position.y - 2, transform.position.z);
             }
+
             _isMoving = true;
         }
     }
@@ -90,11 +95,19 @@ public class PlayerController : MonoBehaviour
 
         if (_elaspedTime <= _moveDuration)
         {
-            transform.position = Vector2.Lerp(_startPosition, _targetPosition, _elaspedTime / _moveDuration);
+            float t = _elaspedTime / _moveDuration;
+            transform.position = Vector3.Lerp(_startPosition, _targetPosition, t);
+            transform.position.Scale(transform.localScale * t);
+
+            //scalePulse multiplies t by Pi inside a sine function. 
+            //This maps the duration of t (from 0 to 1) to a half-circle, causing the pulse to smoothly go from 0 to 1 and back to 0.
+            float scalePulse = Mathf.Sin(t * Mathf.PI);
+            transform.localScale = _originScale * (1f + scalePulse * _scaleMultiplier);
         }
         else
         {
             transform.position = _targetPosition;
+            transform.localScale = _originScale;
             _elaspedTime = 0f;
             _isMoving = false;
         }
